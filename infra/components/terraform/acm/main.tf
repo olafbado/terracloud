3134cc0602d2a574fa21b999690cc8aaf36ed94e
olafbado <niebiel1@gmail.com>
2024-10-28 12:38:10 +0100

    Remove acm component

--- infra/components/terraform/acm/main.tf
+++
@@ -1,6 +0,0 @@
-module "acm_request_certificate" {
-  source                    = "cloudposse/acm-request-certificate/aws"
-  version                   = "0.18.0"
-  domain_name               = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}"]
-}
